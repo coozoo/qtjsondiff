71b7b1bdd89e7ccdbd6bc48d55b71d272369529d
Yuriy Kuzin <realcoozoo@gmail.com>
2017-12-30 13:55:23 +0000

    Add version and name of window to main

--- main.cpp
+++ main.cpp
@@ -6,8 +6,17 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QString platform="";
+    Q_UNUSED(platform);
+    #if __GNUC__
+        #if __x86_64__
+            platform="-64bit";
+        #endif
+    #endif
+    a.setProperty("appversion","0.1b" + platform);
+    a.setProperty("appname","QT JSON Diff");
     MainWindow w;
     w.show();
-
+    w.setWindowTitle(a.property("appname").toString() + " " + a.property("appversion").toString());
     return a.exec();
 }
