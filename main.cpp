b437a03269f0188cfd451d68c1e697fd2fac0123
Iurii Kuzin <IKuzin@on.seo>
2020-01-15 12:39:44 +0000

    Version update
    increment version

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
             platform="-64bit";
         #endif
     #endif
-    a.setProperty("appversion","0.21b" + platform + " (QTbuild:" + QString(QT_VERSION_STR) +")");
+    a.setProperty("appversion","0.22b" + platform + " (QTbuild:" + QString(QT_VERSION_STR) +")");
     a.setProperty("appname","QT JSON Diff");
 
 #ifdef Q_OS_LINUX
