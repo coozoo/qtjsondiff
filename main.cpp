aab7b03916332b8686dc23041ee52a4834241548
Yuriy Kuzin <realcoozoo@gmail.com>
2023-08-10 21:19:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #include <QSettings>
 #include <QtDebug>
 
-const QString APP_VERSION = "0.62b";
+const QString APP_VERSION = "0.63b";
 
 void qtJsonDiffLogger(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
