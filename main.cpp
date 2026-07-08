1f62f6d7be293680869fbf0b0198845342d75bed
happiness <realcoozoo@gmail.com>
2026-07-08 07:04:01 +0300

    version bump

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 #include <QTimer>
 #include <QtDebug>
 
-const QString APP_VERSION = "0.95";
+const QString APP_VERSION = "0.96";
 
 void qtJsonDiffLogger(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
